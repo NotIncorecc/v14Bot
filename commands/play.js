6c2479404e45a78eced8afa83c4ceadf66ecc3ca
NotIncorecc <85992147+NotIncorecc@users.noreply.github.com>
2023-04-12 16:45:00 +0530

    added some play commands
    pause and unpause

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 const { SlashCommandBuilder} = require('discord.js');
-const {joinVoiceChannel, createAudioResource, createAudioPlayer,getVoiceConnection, AudioPlayerStatus, getVoiceConnections} = require('@discordjs/voice');
+const {joinVoiceChannel, createAudioResource, createAudioPlayer,getVoiceConnection, AudioPlayerStatus} = require('@discordjs/voice');
 const {stream} = require("play-dl");
+const audio = new Map();
 
 const ytse = require('yt-search');
 
@@ -15,8 +16,13 @@ module.exports = {
             .addStringOption(option => option.setName('query').setDescription('name of the song')))
         .addSubcommand(subcommand => subcommand
             .setName('leave')
-            .setDescription('The bot stops the music and leaves the channel')
-            )
+            .setDescription('The bot stops the music and leaves the channel'))
+        .addSubcommand(subcommand => subcommand
+            .setName('pause')
+            .setDescription('pauses the audio in the voice channel'))
+        .addSubcommand(subcommand => subcommand
+            .setName('unpause')
+            .setDescription('resumes the audio in the voice channel'))
 
         ,
 	async execute(interaction) {
@@ -32,13 +38,13 @@ module.exports = {
                 const search = await ytse(given_song);
                 const search_res = search.videos[0].url;
 
-                
                 const audioplayer = createAudioPlayer();
 
                 const connection = joinVoiceChannel({
                     channelId:userchannel.channelId,
                     guildId:interaction.guildId,
-                    adapterCreator:interaction.guild.voiceAdapterCreator
+                    adapterCreator:interaction.guild.voiceAdapterCreator,
+                    selfDeaf:false
                 });
 
                 connection.subscribe(audioplayer);
@@ -47,6 +53,8 @@ module.exports = {
 
                 audioplayer.play(createAudioResource(playprocess.stream, {inputType:playprocess.type} ));
 
+                audio.set('music',audioplayer);
+
                 audioplayer.on('error', error => {
                     console.error(`Error: ${error.message} with resource....`);
                 });
@@ -54,16 +62,27 @@ module.exports = {
                 audioplayer.on(AudioPlayerStatus.Idle, (oldState, newState) => {//song ended
                     audioplayer.stop();
                     connection.destroy();
+                    audio.delete('music');
                     return interaction.followUp({content:'Song ended. Leaving channel X('});
                 });
 
                 
                 await interaction.followUp({content:'Song is playing'});
+
             } else if (interaction.options.getSubcommand() === 'leave') {
+
                 const con = getVoiceConnection(interaction.guildId);
                 con.destroy();
                 interaction.followUp({content:'Leaving Channel X('});
-        }
+
+            } else if (interaction.options.getSubcommand() === 'pause') {
+
+                try {audio.get('music').pause(); interaction.followUp({content:'Song paused'});} catch (error) {interaction.followUp({content:'Error in pausing'});}
+
+            } else if (interaction.options.getSubcommand() === 'unpause') {
+
+                try {audio.get('music').unpause(); interaction.followUp({content:'Song unpaused'});} catch (error) {interaction.followUp({content:'Error in unpausing'});}
+            }
 
         } catch (error){
             console.error(error);
