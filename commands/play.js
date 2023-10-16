571659bf3ca500ecce9d4dcdc7b658befaf10fa7
NotIncorecc <85992147+NotIncorecc@users.noreply.github.com>
2023-10-16 15:02:14 +0530

    revising play command
    bot aborts the request (leaves the channel as soon as song starts playing)

--- commands/play.js
+++ commands/play.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 const { SlashCommandBuilder} = require('discord.js');
-const {joinVoiceChannel, createAudioResource, createAudioPlayer,getVoiceConnection, NoSubscriberBehavior, AudioPlayerStatus} = require('@discordjs/voice');
-const play_ = require("play-dl");
+const {joinVoiceChannel, createAudioResource, createAudioPlayer,getVoiceConnection, AudioPlayerStatus} = require('@discordjs/voice');
+const fs = require('fs');
+
 const audio = new Map();
 
-//need to complete authorisation pissed offff will come back later
-
-//const ytse = require('yt-search');
+const ytse = require('yt-search');
+const ytdl = require('ytdl-core');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -15,7 +15,7 @@ module.exports = {
         .addSubcommand(subcommand => subcommand
             .setName('search')
             .setDescription('Enter song you want to search and play in channel')
-            .addStringOption(option => option.setName('query').setDescription('name of the song').setRequired(true)))
+            .addStringOption(option => option.setName('query').setDescription('name of the song')))
         .addSubcommand(subcommand => subcommand
             .setName('leave')
             .setDescription('The bot stops the music and leaves the channel'))
@@ -30,63 +30,38 @@ module.exports = {
 	async execute(interaction) {
         
         try{
-            await interaction.deferReply();
-
-            const userchannel = interaction.member.voice;
-            if (!userchannel) {return interaction.followUp('You must be in a voice channel to use this command');}
-
             if (interaction.options.getSubcommand() === 'search')  {
-                if (audio.get('music')) {return interaction.followUp('A song is already playing in the vc');}
                 const given_song = interaction.options.getString('query');
-                //const search = await ytse(given_song);
-                //const search_res = search.videos[0].url;
+                await interaction.deferReply();
 
-                const audioplayer = createAudioPlayer({
-                    behaviors: {
-                        noSubscriber: NoSubscriberBehavior.Play
-                    }
-                });
-
+                const search = await ytse(given_song);
+                const search_res = search.videos[0].url;
+    
+                const userchannelid = interaction.member.voice.channelId;
+                console.log(userchannelid);
+                if (!userchannelid) {return interaction.followUp('You must be in a voice channel to use this command');}
+    
+                const audioplayer = createAudioPlayer();
+    
                 const connection = joinVoiceChannel({
-                    channelId:userchannel.channelId,
+                    channelId:userchannelid,
                     guildId:interaction.guildId,
-                    adapterCreator:interaction.guild.voiceAdapterCreator,
-                    selfDeaf:false
+                    adapterCreator:interaction.guild.voiceAdapterCreator
                 });
-/*
-                if (play.is_expired()) {
-                    await play.refreshToken(); // This will check if access token has expired or not. If yes, then refresh the token.
-                }*/
-
+    
                 connection.subscribe(audioplayer);
-
-                //const playprocess = await stream(search_res);
-
-                let sp_data = await play_.spotify(given_song); // This will get spotify data from the url [ I used track url, make sure to make a logic for playlist, album ]
-        
-                let searched = await play_.search(`${sp_data.name}`, {
-                    limit: 1
-                }) ;// This will search the found track on youtube.
-
-                let stream = await play_.stream(searched[0].url); // This will create stream from the above search
-
-                audioplayer.play(createAudioResource(stream.stream, {inputType:stream.type} ));
-
-                audio.set('music',audioplayer);
-
-                audioplayer.on('error', error => {
-                    console.error(`Error: ${error.message} with resource....`);
-                });
-
-                audioplayer.on(AudioPlayerStatus.Idle, (oldState, newState) => {//song ended
-                    audioplayer.stop();
-                    connection.destroy();
-                    audio.delete('music');
-                    return interaction.followUp({content:'Song ended. Leaving channel X('});
-                });
-
+    
+                const ytdlprocess = ytdl(search_res, {filter:'audioonly'}).pipe(fs.createWriteStream('mus.mp3'));
+                ytdlprocess.on("error",(error) => {console.error(error);});
                 
+                audioplayer.play(createAudioResource(ytdlprocess));
+    
                 await interaction.followUp({content:'Song is playing'});
+    
+                audioplayer.on(AudioPlayerStatus.Idle, (oldState, newState) => {//song ended
+                    connection.destroy();
+                    return interaction.followUp({content:'Song ended. Leaving channel'});
+                });
 
             } else if (interaction.options.getSubcommand() === 'leave') {
 
